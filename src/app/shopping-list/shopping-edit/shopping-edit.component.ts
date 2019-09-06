3b454a73a2558a0a3316759463664c732c50cc19
mpsinghk <mpsinghk@users.noreply.github.com>
2019-09-06 00:07:04 -0700

    16-218: Allowing the selection of items in the list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -9,11 +10,20 @@ import { ShoppingListService } from '../shopping-list.service';
     templateUrl: './shopping-edit.component.html',
     styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+    editMode = false;
+    editedItemIndex: number;
+    subscription: Subscription;
 
     constructor(private slService: ShoppingListService) { }
 
     ngOnInit() {
+        this.subscription = this.slService.startedEditing.subscribe(
+            (index: number) => {
+                this.editedItemIndex = index;
+                this.editMode = true;
+            }
+        );
     }
 
     onAddItem(form: NgForm) {
@@ -23,4 +33,7 @@ export class ShoppingEditComponent implements OnInit {
         this.slService.addIngredient(newIngredient);
     }
 
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 }
