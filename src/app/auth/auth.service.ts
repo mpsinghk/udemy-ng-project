f936a233e26d578d167ebf78f1dd259dc90be156
mpsinghk <mpsinghk@users.noreply.github.com>
2019-10-15 23:43:42 -0700

    20-294: Improving error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 interface AuthResponseData {
     idToken: string;
@@ -14,15 +16,30 @@ export class AuthService {
     constructor(private http: HttpClient) {}
 
     signup(email: string, password: string) {
-        return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAh0dY4hAgp5dYgSukQ_ZKQ-nhSQosRgmo',
-            {
-                // tslint:disable-next-line: object-literal-shorthand
-                email: email,
-                // tslint:disable-next-line: object-literal-shorthand
-                password: password,
-                returnSecureToken: true
-            }
-        );
+        return this.http
+            .post<AuthResponseData>(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAh0dY4hAgp5dYgSukQ_ZKQ-nhSQosRgmo',
+                {
+                    // tslint:disable-next-line: object-literal-shorthand
+                    email: email,
+                    // tslint:disable-next-line: object-literal-shorthand
+                    password: password,
+                    returnSecureToken: true
+                }
+            )
+            .pipe(
+                catchError(errorRes => {
+                    let errorMessage = 'An unknown error occurred!';
+                    if (!errorRes.error || !errorRes.error.error) {
+                        return throwError(errorMessage);
+                    }
+                    switch (errorRes.error.error.message) {
+                        case 'EMAIL_EXISTS':
+                            errorMessage = 'This email already exists!';
+                    }
+
+                    return throwError(errorMessage);
+                })
+            );
     }
 }
