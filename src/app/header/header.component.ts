c8bf538a78f29b59cc1edd00043d18eadf83fe36
mpsinghk <mpsinghk@users.noreply.github.com>
2019-10-17 23:30:03 -0700

    20-298: Reflecting the auth state in the UI

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
     collapsed = true;
+    isAuthenticated = false;
+    private userSub: Subscription;
 
-    constructor(private dataStorageService: DataStorageService) {}
+    constructor(
+        private dataStorageService: DataStorageService,
+        private authService: AuthService
+    ) {}
+
+    ngOnInit() {
+        this.userSub = this.authService.user.subscribe(user => {
+            // this.isAuthenticated = !user ? false : true;
+            // same thing as the following
+            this.isAuthenticated = !!user;
+        });
+    }
 
     onSaveData() {
         this.dataStorageService.storeRecipes();
@@ -18,5 +33,9 @@ export class HeaderComponent {
 
     onFetchData() {
         this.dataStorageService.fetchRecipes().subscribe();
+    }
+
+    ngOnDestroy() {
+        this.userSub.unsubscribe();
     }
 }
