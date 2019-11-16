c65eefd06edc530f8558fd8019415020168faa59
mpsinghk <mpsinghk@users.noreply.github.com>
2019-11-15 21:31:45 -0800

    24-349: Understanding and adding actions

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,6 +1,7 @@
 import { Action } from '@ngrx/store';
 
-import { Ingredient } from '../shared/ingredient.model';
+import { Ingredient } from '../../shared/ingredient.model';
+import { ADD_INGREDIENT } from './shopping-list.actions';
 
 const initialState = {
     ingredients: [new Ingredient('Apple', 5), new Ingredient('Tometoes', 10)]
@@ -8,7 +9,7 @@ const initialState = {
 
 export function shoppingListReducer(state = initialState, action: Action) {
     switch (action.type) {
-        case 'ADD_INGREDIENT':
+        case ADD_INGREDIENT:
             return {
                 ...state,
                 ingredients: [...state.ingredients, action]
