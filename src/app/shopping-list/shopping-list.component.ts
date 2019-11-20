deb4f794adb73389e5f138a32ec8d61ce4bfeb23
mpsinghk <mpsinghk@users.noreply.github.com>
2019-11-19 21:49:02 -0800

    24-356: Expanding the State

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import * as fromShoppingList from './store/shopping-list.reducer';
 
 @Component({
     selector: 'app-shopping-list',
@@ -17,7 +18,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
     constructor(
         private slService: ShoppingListService,
-        private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+        private store: Store<fromShoppingList.AppState>
     ) {}
 
     ngOnInit() {
