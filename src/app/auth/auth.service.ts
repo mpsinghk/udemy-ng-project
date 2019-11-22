b865eeda8fa4a32f5c9bcb44757e5182b8f8e6a9
mpsinghk <mpsinghk@users.noreply.github.com>
2019-11-21 22:12:15 -0800

    24-362: Dispatching auth actions

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,9 +3,12 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { catchError, tap } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
+import { Store } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 
 import { User } from './user.model';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 export interface AuthResponseData {
     idToken: string;
@@ -21,7 +24,11 @@ export class AuthService {
     user = new BehaviorSubject<User>(null);
     private tokenExpirationTimer: any;
 
-    constructor(private http: HttpClient, private router: Router) {}
+    constructor(
+        private http: HttpClient,
+        private router: Router,
+        private store: Store<fromApp.AppState>
+    ) {}
 
     signup(email: string, password: string) {
         return (
@@ -99,7 +106,15 @@ export class AuthService {
         );
 
         if (loadedUser.token) {
-            this.user.next(loadedUser);
+            // this.user.next(loadedUser);
+            this.store.dispatch(
+                new AuthActions.Login({
+                    email: loadedUser.email,
+                    userId: loadedUser.id,
+                    token: loadedUser.token,
+                    expirationDate: new Date(userData._tokenExpirationDate)
+                })
+            );
             const expirationDuration =
                 new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
             this.autoLogout(expirationDuration);
@@ -107,7 +122,8 @@ export class AuthService {
     }
 
     logout() {
-        this.user.next(null);
+        // this.user.next(null);
+        this.store.dispatch(new AuthActions.Logout());
         this.router.navigate(['/auth']);
         localStorage.removeItem('userData');
         if (this.tokenExpirationTimer) {
@@ -130,7 +146,19 @@ export class AuthService {
     ) {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
-        this.user.next(user);
+        // this.user.next(user);
+        this.store.dispatch(
+            new AuthActions.Login({
+                // tslint:disable-next-line: object-literal-shorthand
+                email: email,
+                // tslint:disable-next-line: object-literal-shorthand
+                userId: userId,
+                // tslint:disable-next-line: object-literal-shorthand
+                token: token,
+                // tslint:disable-next-line: object-literal-shorthand
+                expirationDate: expirationDate
+            })
+        );
         this.autoLogout(expiresIn * 1000);
         localStorage.setItem('userData', JSON.stringify(user));
     }
