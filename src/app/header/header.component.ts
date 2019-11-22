f8f44d72fd71f4dcb5b1629f7b1a1a4fe7a6b8d3
mpsinghk <mpsinghk@users.noreply.github.com>
2019-11-21 23:13:19 -0800

    24-363: Auth finnished (for now...)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
+import * as fromApp from '../store/app.reducer';
 
 @Component({
     selector: 'app-header',
@@ -16,15 +19,20 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
     constructor(
         private dataStorageService: DataStorageService,
-        private authService: AuthService
+        private authService: AuthService,
+        private store: Store<fromApp.AppState>
     ) {}
 
     ngOnInit() {
-        this.userSub = this.authService.user.subscribe(user => {
-            // this.isAuthenticated = !user ? false : true;
-            // same thing as the following
-            this.isAuthenticated = !!user;
-        });
+        // this.userSub = this.authService.user.subscribe(user => {
+        this.userSub = this.store
+            .select('auth')
+            .pipe(map(authState => authState.user))
+            .subscribe(user => {
+                // this.isAuthenticated = !user ? false : true;
+                // same thing as the following
+                this.isAuthenticated = !!user;
+            });
     }
 
     onSaveData() {
